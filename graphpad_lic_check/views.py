dd247d3d5b0147538a88936efd126cdec69e3aad
Luc Galarneau <lgalarno@gmail.com>
2019-06-05 18:02:34 -0400

    gp act code check requestKey

--- graphpad_lic_check/views.py
+++ graphpad_lic_check/views.py
@@ -1,11 +1,32 @@
-from django.shortcuts import render
 
-# Create your views here.
-from django.http import JsonResponse,JsonMinResponse
 
+from django.core.serializers.json import DjangoJSONEncoder
+from django.http import HttpResponse
+
+import json
 import datetime
 import hashlib
 
+class JsonMinResponse(HttpResponse):
+    """
+    An HTTP response class that consumes data to be serialized to JSON.
+
+    :param data: Data to be dumped into json. By default only ``dict`` objects
+      are allowed to be passed due to a security flaw before EcmaScript 5. See
+      the ``safe`` parameter for more information.
+    :param encoder: Should be a json encoder class. Defaults to
+      ``django.core.serializers.json.DjangoJSONEncoder``.
+    :param safe: Controls if only ``dict`` objects may be serialized. Defaults
+      to ``True``.
+    :param json_dumps_params: A dictionary of kwargs passed to json.dumps().
+    """
+    def __init__(self, data, encoder=DjangoJSONEncoder, safe=True, **kwargs):
+        if safe and not isinstance(data, dict):
+            raise TypeError('In order to allow non-dict objects to be '
+                'serialized set the safe parameter to False')
+        kwargs.setdefault('content_type', 'application/json')
+        data =  json.dumps(data, separators = (',', ':'), cls=encoder)   #json.dumps(data, separators = (',', ':')), cls=encoder)
+        super(JsonMinResponse, self).__init__(content=data, **kwargs)
 
 
 def license_check(request, act_code=None):
