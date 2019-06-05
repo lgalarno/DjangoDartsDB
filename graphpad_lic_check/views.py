1d81d9b8c54f91c2ebf9177168f1f40d3b126aeb
Luc Galarneau <lgalarno@gmail.com>
2019-06-05 03:05:40 -0400

    gp act code check requestKey

--- graphpad_lic_check/views.py
+++ graphpad_lic_check/views.py
@@ -4,8 +4,7 @@ from django.shortcuts import render
 from django.http import JsonResponse,JsonMinResponse
 
 import datetime
-import json
-import secrets
+import hashlib
 
 
 
@@ -14,22 +13,12 @@ def license_check(request, act_code=None):
     List all code snippets, or create a new snippet.
     """
     # print(request)
-    # requestID = request.GET.get('requestID', '')
-    # print(requestID)
-    # print( act_code)
+    tohash = act_code+request.GET.get('requestID', '')+"kWNVrSrMHfH4KpIVcs8bSheLs4M"
+    rk = hashlib.md5(tohash.encode('utf-8')).hexdigest().upper()
     d = datetime.date(2028,12,31)
-    # return StreamingHttpResponse(JsonResponse({'requestKey': secrets.token_hex(10).upper(), #'E173663A785B72C4D75E',
-    #                          'activationCode': act_code, #'ACTGP-8C8246DA-DC2899AB-A2983DB6-052A6C01',
-    #                          'expirationDate': d.strftime("%m/%d/%Y")
-    #                          }), content_type='application/json')
-    return JsonMinResponse({'requestKey': secrets.token_hex(10).upper(), #'E173663A785B72C4D75E',
+
+    return JsonMinResponse({'requestKey': rk[0:20], #'E173663A785B72C4D75E',
                              'activationCode': act_code, #'ACTGP-8C8246DA-DC2899AB-A2983DB6-052A6C01',
                              'expirationDate': d.strftime("%m/%d/%Y")
                              })
 
-# @api_view(['GET', 'POST'])
-# def license_check2(request, act_code=None):
-#     print(act_code)
-#     if request.method == 'POST':
-#         return Response({"message": "Got some data!", "data": request.data})
-#     return Response({"message": "Hello, world!"})
